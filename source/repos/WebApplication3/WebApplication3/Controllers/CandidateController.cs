c012f6516b359fecb5309f37ca24268e140ec7b2
Raymund De La Victoria <raymunddlv@gmail.com>
2022-06-02 13:21:33 +0800

    added the API

---
+++ source/repos/WebApplication3/WebApplication3/Controllers/CandidateController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CandidateController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"select candidate_id, first_name, last_name, email, phone_num, address_, country from dbo.Candidate";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CandidateAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(Candidate can)
+        {
+
+            string query = @"insert into dbo.Candidate 
+                           (first_name, last_name, email, phone_num, address_, country)
+                           values ('"+can.first_name+ @"', '" + can.last_name + @"', '" + can.email + @"', '" + can.phone_num + @"', '" + can.address_ + @"',
+                           '" + can.country + @"')";
+
+            //string query = @"select candidate_id, first_name, last_name, email, phone_num, address_, country from dbo.Candidate";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CandidateAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully!");
+        }
+
+        [HttpPut]
+
+        public JsonResult Put(Candidate can)
+        {
+
+            string query = @"update dbo.Candidate set
+                           first_name = '" + can.first_name + @"',
+                           last_name = '" + can.last_name + @"',
+                           email = '" + can.email + @"',
+                           phone_num = '" + can.phone_num + @"',
+                           address_ = '" + can.address_ + @"',
+                           country = '" + can.country + @"'
+                           where candidate_id = " + can.candidate_id + @"";
+            
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CandidateAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully!");
+        }
+
+        [HttpDelete("{id}")]
+
+        public JsonResult Delete(int id)
+        {
+
+            string query = @"delete from dbo.Candidate where candidate_id = " + id + @"";
+
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CandidateAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully!");
+        }
+
+    }
+}
---
+++ source/repos/WebApplication3/WebApplication3/Models/Candidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Candidate
+    {
+        public int candidate_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string phone_num { get; set; }
+        public string address_ { get; set; }
+        public string country { get; set; }
+    }
+}
